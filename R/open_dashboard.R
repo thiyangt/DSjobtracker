fb7df9243d8de06455ea6fd8fa3c1b124e468619
Janith Wanniarachchi <janithcwanni@gmail.com>
2020-10-06 11:44:45 +0530

    added open dashboard function

---
+++ R/open_dashboard.R
@@ -0,0 +1,6 @@
+open_dashboard <- function() {
+#' Opens the dashboard containing visualizations of the dataset
+  utils::browseURL(
+    "https://scs-fas-sjp.netlify.app/dsjobtrackervisualisation/dataviz"
+  )
+}
